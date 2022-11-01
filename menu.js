78b499be7bcb88426fcdd4a98e02bf451ed238b3
Arjun Enkhbat <91604993+nujra122@users.noreply.github.com>
2022-11-01 11:35:55 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,14 @@
-const openMenu = document.getElementsByClassName("aside");
-const closeMenu = document.querySelector(".x");
-console.log(openMenu, "menu");
-openMenu.addEventListener("onClick", function(ev){
-    console.log("kk");
+const openMenu = document.getElementById("menuBtn");
+const closeMenu = document.getElementById("xclose");
+
+openMenu.addEventListener("click", function(ev){
+    console.log('asdfasf');
+    document.getElementById('myaside').classList.add('newAside');
+    openMenu.classList.toggle("active");
+    closeMenu.classList.toggle("active");
+})
+closeMenu.addEventListener("click", function(ev){
+    document.getElementById('myaside').classList.remove('newAside');
     openMenu.classList.toggle("active");
     closeMenu.classList.toggle("active");
 })
